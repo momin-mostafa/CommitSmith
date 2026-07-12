95149761e1193c61778ed7a3040023b02525a03b
momin-mostafa <mominmostafa.g@gmail.com>
2026-07-13 02:43:02 +0600

    feat(build): update go command for main package

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Building git_comment..."
-go build -o git_comment ./cmd/git_comment
+go build -o git_comment ./cmd/git_comment_main
 if [ $? -eq 0 ]; then
     echo "✓ Build successful: ./git_comment"
 else
